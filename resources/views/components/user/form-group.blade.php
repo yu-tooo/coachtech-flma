578ea4c79411efae72a2e16adbaea70cc778b584
yu-tooo <teg.moritayuto@gmail.com>
2023-06-22 23:00:31 +0900

    update input component

--- resources/views/components/user/form-group.blade.php
+++ resources/views/components/user/form-group.blade.php
@@ -2,10 +2,11 @@
 'name',
 'labelName',
 'type' => 'text',
-'pattern' => '.*'
+'pattern' => '.*',
+'placeholder' => ''
 ])
 
 <div class="w-full mb-8">
   <label class="block text-lg font-bold cursor-pointer hover:text-gray-600" for="{{ $name }}">{{ $labelName }}</label>
-  <input class="w-full rounded-md focus:ring-0" type="{{ $type }}" id="{{ $name }}" name="{{ $name }}" pattern="{{ $pattern }}">
+  <input class="w-full rounded-md focus:ring-0" type="{{ $type }}" id="{{ $name }}" name="{{ $name }}" pattern="{{ $pattern }}" placeholder="{{ $placeholder }}">
 </div>
--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,11 +1,11 @@
 <x-user-login>
   <h1 class="mb-8 text-2xl font-extrabold">ログイン</h1>
 
-  <x-user.form-group name="email" labelName="メールアドレス" type="email" />
+  <x-user.form-group name="email" labelName="メールアドレス" type="email"/>
 
-  <x-user.form-group name="password" labelName="パスワード" type="password" />
+  <x-user.form-group name="password" labelName="パスワード" type="password"/>
 
-  <x-user.button name="ログインする" />
+  <x-user.button name="ログインする"/>
   
   <a class="mt-6 text-blue-500" href="{{ route('user.register') }}">会員登録はこちら</a>
 </x-user-login>
