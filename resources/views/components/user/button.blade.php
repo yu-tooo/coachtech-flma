5d01bc1c6c0b24847dec76d9523cb91131918977
yu-tooo <teg.moritayuto@gmail.com>
2023-06-22 16:41:11 +0900

    created button component

---
+++ resources/views/components/user/button.blade.php
@@ -0,0 +1,5 @@
+@props([
+  'name'
+])
+
+<button class="w-full mt-12 p-2 text-white text-lg rounded-md bg-red-500 hover:bg-white hover:text-red-500 hover:border hover:border-red-500">{{ $name }}</button>
