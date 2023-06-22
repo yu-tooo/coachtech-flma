b7d184f7264e758b93cb61d57f0e52a33b3ac6c7
yu-tooo <teg.moritayuto@gmail.com>
2023-06-22 23:44:44 +0900

    create sell view

---
+++ resources/views/user/sell.blade.php
@@ -0,0 +1,23 @@
+<x-user-entry>
+  <h1 class="mb-8 text-2xl font-extrabold">商品の出品</h1>
+
+  <form class="w-full" action="/">
+
+    <x-user.form-group name="image" labelName="商品画像" placeholder="要変更！" />
+
+    <x-user.partition name="商品の詳細" />
+    <x-user.form-group name="category" labelName="カテゴリー" />
+    <x-user.form-group name="status" labelName="商品の状態" />
+
+    <x-user.partition name="商品名と説明" />
+    <x-user.form-group name="name" labelName="商品名" />
+    <label class="block text-lg font-bold cursor-pointer hover:text-gray-600" for="description">商品の説明</label>
+    <textarea class="w-full rounded-md focus:ring-0 resize-none mb-8" name="description" id="description" rows="3"></textarea>
+
+    <x-user.partition name="販売価格" />
+    <x-user.form-group name="price" labelName="販売価格" placeholder="￥" />
+
+    <x-user.button name="出品する" />
+  </form>
+
+</x-user-entry>
