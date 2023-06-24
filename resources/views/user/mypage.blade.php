f9604ae94204c5b987fea5d9b012ff20e95e191f
yu-tooo <teg.moritayuto@gmail.com>
2023-06-24 15:11:48 +0900

    create view mypage

--- resources/views/user/mypage.blade.php
+++ resources/views/user/mypage.blade.php
@@ -1,14 +1,74 @@
 <x-user-catalog>
-  <div class="lg:px-32">
-    <div class="flex flex-col justify-between items-center sm:flex-row">
-      <div class="flex flex-col items-center sm:ml-32 md:flex-row">
-        <img class="w-24 h-24 rounded-full" src="{{ asset('storage/img/person.png') }}">
-        <p class="text-xl font-extrabold md:translate-x-4 md:-translate-y-2">ユーザー名</p>
-      </div>
+  <div x-data="{ isSecond: false }">
+    <div class="lg:px-32">
+      <div class="flex flex-col justify-between items-center sm:flex-row">
+        <div class="flex flex-col items-center sm:ml-32 md:flex-row">
+          <img class="w-24 h-24 rounded-full" src="{{ asset('storage/img/person.png') }}">
+          <p class="text-xl font-extrabold md:translate-x-8 md:-translate-y-2">ユーザー名</p>
+        </div>
 
-      <div class="mt-4 w-48 sm:mt-0 sm:mr-24">
-        <x-user.edit-button name="プロフィールを編集" />
+        <div class="mt-4 w-48 sm:mt-0 sm:mr-24">
+          <x-user.edit-button name="プロフィールを編集" />
+        </div>
+      </div>
+    </div>
+
+    <div class="w-full border-b-2 border-gray-400 mt-8 px-8 py-2 md:px-16 lg:px-32">
+      <button @click="isSecond = false" class="mx-4 font-bold outline-none" :class="{'text-red-500': ! isSecond }">出品した商品</button>
+      <button @click="isSecond = true" class="mx-4 font-bold outline-none" :class="{'text-red-500': isSecond }">購入した商品</button>
+    </div>
+
+
+    <!-- 出品商品 -->
+    <div class="grid mt-8 gap-8 px-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:gap-16 lg:px-32" :class="{'hidden': isSecond }">
+      <div class="border-2 max-w-xs mx-auto">
+        <img src="{{ asset('storage/img/exhibit.png') }}">
+      </div>
+      <div class="border-2 max-w-xs mx-auto">
+        <img src="{{ asset('storage/img/exhibit.png') }}">
+      </div>
+      <div class="border-2 max-w-xs mx-auto">
+        <img src="{{ asset('storage/img/exhibit.png') }}">
+      </div>
+      <div class="border-2 max-w-xs mx-auto">
+        <img src="{{ asset('storage/img/exhibit.png') }}">
+      </div>
+      <div class="border-2 max-w-xs mx-auto">
+        <img src="{{ asset('storage/img/exhibit.png') }}">
+      </div>
+      <div class="border-2 max-w-xs mx-auto">
+        <img src="{{ asset('storage/img/exhibit.png') }}">
+      </div>
+      <div class="border-2 max-w-xs mx-auto">
+        <img src="{{ asset('storage/img/exhibit.png') }}">
+      </div>
+      <div class="border-2 max-w-xs mx-auto">
+        <img src="{{ asset('storage/img/exhibit.png') }}">
+      </div>
+      <div class="border-2 max-w-xs mx-auto">
+        <img src="{{ asset('storage/img/exhibit.png') }}">
+      </div>
+    </div>
+
+
+    <!-- 購入商品 -->
+    <div class="grid mt-8 gap-8 px-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:gap-16 lg:px-32" :class="{'hidden': ! isSecond }">
+      <div class="border-2 max-w-xs mx-auto">
+        <img src="{{ asset('storage/img/purchase.png') }}">
+      </div>
+      <div class="border-2 max-w-xs mx-auto">
+        <img src="{{ asset('storage/img/purchase.png') }}">
+      </div>
+      <div class="border-2 max-w-xs mx-auto">
+        <img src="{{ asset('storage/img/purchase.png') }}">
+      </div>
+      <div class="border-2 max-w-xs mx-auto">
+        <img src="{{ asset('storage/img/purchase.png') }}">
+      </div>
+      <div class="border-2 max-w-xs mx-auto">
+        <img src="{{ asset('storage/img/purchase.png') }}">
       </div>
     </div>
   </div>
+
 </x-user-catalog>
