b10591231d41f70d1d491e1b6049b23461da8a26
yu-tooo <teg.moritayuto@gmail.com>
2023-06-25 17:46:28 +0900

    sort out controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 // use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
