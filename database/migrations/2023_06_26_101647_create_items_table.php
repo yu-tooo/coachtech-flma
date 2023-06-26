19328f7eaf2ce1181d3ea58601c7ffcb326a6f74
yu-tooo <teg.moritayuto@gmail.com>
2023-06-26 11:16:43 +0900

    create database data

---
+++ database/migrations/2023_06_26_101647_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('price', 255);
+            $table->string('description', 255);
+            $table->string('img_url', 255);
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_good');
+            $table->unsignedBigInteger('condition_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+};
---
+++ database/migrations/2023_06_26_101814_create_profiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('img_url', 255);
+            $table->string('postcode', 255);
+            $table->string('address', 255);
+            $table->string('building', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
