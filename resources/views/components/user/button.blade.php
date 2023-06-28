a488ea8f24c3d40d6f93cba18c1daf1caeb4c818
yu-tooo <teg.moritayuto@gmail.com>
2023-06-28 17:51:02 +0900

    update button

--- resources/views/components/user/button.blade.php
+++ resources/views/components/user/button.blade.php
@@ -1,5 +1,5 @@
 @props([
-  'name' => 'ボタン'
+'name' => 'ボタン'
 ])
 
-<button class="w-full p-2 text-white text-lg rounded-md bg-red-500 hover:bg-white hover:text-red-500 hover:border hover:border-red-500">{{ $name }}</button>
+<button class="w-full p-2 text-white text-lg rounded-md bg-red-500 border border-red-500 hover:bg-white hover:text-red-500 ">{{ $name }}</button>
