169369fcfa23191263b80afb49fc8d7fa731a7af
yu-tooo <teg.moritayuto@gmail.com>
2023-06-28 20:22:37 +0900

    bug fix likeseeder.php

--- database/seeders/LikeSeeder.php
+++ database/seeders/LikeSeeder.php
@@ -17,7 +17,7 @@ class LikeSeeder extends Seeder
     {
         for ($i = 1; $i <= 7; $i++) {
             $param = [];
-            $param['user_id'] = ($i % 5) + 1;
+            $param['user_id'] = 6 - (($i % 5) + 1);
             $param['item_id'] = $i;
             Like::create($param);
         }
