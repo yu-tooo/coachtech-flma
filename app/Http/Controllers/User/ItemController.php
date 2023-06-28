6fffe9fd3b76edca3c34b62f13b92beb993f3904
yu-tooo <teg.moritayuto@gmail.com>
2023-06-28 13:21:23 +0900

    add catalog view

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
     public function index()
     {
-        return view('user.index');
+        $items = Item::all();
+        return view('user.index', [ 'items' => $items]);
     }
     
     public function detail()
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -5,39 +5,17 @@
   </div>
 
   <!-- 出品商品 -->
-  <div x-show="! isSecond" class="grid my-8 gap-8 px-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:gap-16 lg:px-32">
-    <div class="border-2 max-w-xs mx-auto">
-      <img src="{{ asset('storage/image/exhibit.png') }}">
-    </div>
-    <div class="border-2 max-w-xs mx-auto">
-      <img src="{{ asset('storage/image/exhibit.png') }}">
-    </div>
-    <div class="border-2 max-w-xs mx-auto">
-      <img src="{{ asset('storage/image/exhibit.png') }}">
-    </div>
-    <div class="border-2 max-w-xs mx-auto">
-      <img src="{{ asset('storage/image/exhibit.png') }}">
-    </div>
-    <div class="border-2 max-w-xs mx-auto">
-      <img src="{{ asset('storage/image/exhibit.png') }}">
-    </div>
-    <div class="border-2 max-w-xs mx-auto">
-      <img src="{{ asset('storage/image/exhibit.png') }}">
-    </div>
-    <div class="border-2 max-w-xs mx-auto">
-      <img src="{{ asset('storage/image/exhibit.png') }}">
-    </div>
-    <div class="border-2 max-w-xs mx-auto">
-      <img src="{{ asset('storage/image/exhibit.png') }}">
-    </div>
-    <div class="border-2 max-w-xs mx-auto">
-      <img src="{{ asset('storage/image/exhibit.png') }}">
-    </div>
+  <div x-show="! isSecond" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-8 px-12 xl:px-20 py-12">
+    @foreach ($items as $item)
+    <a href="">
+      <img class="w-full h-48 shadow-xl object-cover" src="{{ asset('storage/image/'. $item->img_url) }}">
+    </a>
+    @endforeach
   </div>
 
 
   <!-- 購入商品 -->
-  <div x-show="isSecond" class="grid my-8 gap-8 px-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:gap-16 lg:px-32">
+  <!-- <div x-show="isSecond" class="grid my-12 gap-4 px-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:gap-16 lg:px-32">
     <div class="border-2 max-w-xs mx-auto">
       <img src="{{ asset('storage/image/purchase.png') }}">
     </div>
@@ -53,5 +31,5 @@
     <div class="border-2 max-w-xs mx-auto">
       <img src="{{ asset('storage/image/purchase.png') }}">
     </div>
-  </div>
+  </div> -->
 </x-user-catalog>
