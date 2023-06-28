373ecf08fb51839e8273283553cc8f3fe107b860
yu-tooo <teg.moritayuto@gmail.com>
2023-06-28 19:31:46 +0900

    update migration and seed

---
+++ database/seeders/LikeSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LikeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i <= 7; $i++) {
+            $param = [];
+            $param['user_id'] = ($i % 5) + 1;
+            $param['item_id'] = $i;
+        }
+    }
+}
