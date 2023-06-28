eba66e4468626337c1c9a82cd7d6c3f82bba0448
yu-tooo <teg.moritayuto@gmail.com>
2023-06-28 18:36:35 +0900

    update address

--- resources/views/user/item.blade.php
+++ resources/views/user/item.blade.php
@@ -19,24 +19,24 @@
       </div>
     </div>
 
-    <form class="w-full mt-4" action="{{ route('user.purchase', ['item_id' => $item->id]) }}">
+    <a class="w-full mt-4" href="{{ route('user.purchase', ['item_id' => $item->id]) }}">
       <x-user.button name="購入する" />
-    </form>
+    </a>
 
     <h2 class="text-xl font-bold pt-8">商品説明</h2>
     <p>{{ $item->description }}</p>
-    <h2 class="text-xl font-bold pt-8">商品の情報</h2>
-    <table class="table-fixed border-separate border-spacing-x-1 border-spacing-y-4">
-      <tr>
-        <th class="w-2/5">カテゴリー</th>
-        <td class="bg-gray-300 text-sm rounded-2xl px-2">洋服</td>
-        <td class="bg-gray-300 text-sm rounded-2xl px-2">メンズ</td>
-        <td class="bg-gray-300 text-sm rounded-2xl px-2">メンズ</td>
-      </tr>
-      <tr>
-        <th>商品の状態</th>
-        <td>良好</td>
-      </tr>
-    </table>
+    <h2 class=" text-xl font-bold pt-8">商品の情報</h2>
+      <table class="table-fixed border-separate border-spacing-x-1 border-spacing-y-4">
+        <tr>
+          <th class="w-2/5">カテゴリー</th>
+          <td class="bg-gray-300 text-sm rounded-2xl px-2">洋服</td>
+          <td class="bg-gray-300 text-sm rounded-2xl px-2">メンズ</td>
+          <td class="bg-gray-300 text-sm rounded-2xl px-2">メンズ</td>
+        </tr>
+        <tr>
+          <th>商品の状態</th>
+          <td>良好</td>
+        </tr>
+      </table>
   </x-slot>
 </x-user-item-detail>
