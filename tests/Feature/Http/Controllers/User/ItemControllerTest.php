db8cbffb2c90186081b240b43b79fc64d9c954e5
yu-tooo <teg.moritayuto@gmail.com>
2023-07-09 14:22:52 +0900

    update models test

--- tests/Feature/Http/Controllers/User/ItemControllerTest.php
+++ tests/Feature/Http/Controllers/User/ItemControllerTest.php
@@ -3,9 +3,10 @@
 namespace Tests\Feature\Http\Controllers\User;
 
 use App\Models\Item;
+use App\Models\User;
 use Database\Seeders\ItemSeeder;
+use Database\Seeders\UserSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ItemControllerTest extends TestCase
@@ -14,40 +15,39 @@ class ItemControllerTest extends TestCase
     /** @test */
     function index_view(): void
     {
-        $this->seed([
-            ItemSeeder::class,
-        ]);
-        $this->get(route('user.home'))
-        ->assertStatus(200)
-        ->assertSee('items/item12');
+        $this->seed([ItemSeeder::class]);
+        $this->get(route('user.home'))->assertStatus(200)->assertSee('item12');
 
         $this->get(route('user.home', [
             'name' => 'ネックレス'
-        ]))->assertStatus(200)
-        ->assertSee('items/item7')
-        ->assertDontSee('items/item12');
+        ]))->assertStatus(200)->assertSee('item7')->assertDontSee('item12');
     }
 
     /** @test */
-    function item_view() {
+    function index_cannot_see_own_items(): void
+    {
+        $this->seed([ItemSeeder::class, UserSeeder::class]);
+        $user = User::find(1);
+
+        $this->actingAs($user, 'users')->get(route('user.home'))
+        ->assertSee('item1')
+        ->assertDontSee('item5');
+    }
+
+    /** @test */
+    function item_view(): void
+    {
         $this->seed();
-        $item1 = Item::find(1);
-        $item2 = Item::find(2);
+        $item1 = Item::withCount('like')->withCount('comment')->find(1);
 
         $this->get(route('user.item', ['item_id' => $item1]))
-        ->assertStatus(200)
-        ->assertSee($item1->name)
-        ->assertSee('￥5,400(値段)')
-        ->assertSee(1)
-        ->assertSee(3)
-        ->assertSee('装着')
-        ->assertSee('運動');
-
-        $this->get(route('user.item', ['item_id' => $item2]))
-        ->assertStatus(200)
-        ->assertSee($item2->name)
-        ->assertSee('￥1,200(値段)')
-        ->assertSee('学生');
-    }
-    
+        ->assertStatus(200)       
+        ->assertSeeText([
+            $item1->name, 
+            number_format($item1->price), 
+            $item1->like_count,
+            $item1->comment_count,
+            $item1->condition->getCondition()
+        ])->assertSeeInOrder(['装着', '運動']);
+    }    
 }
--- tests/Feature/Models/ItemTest.php
+++ tests/Feature/Models/ItemTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature\Models;
 
-use App\Models\Condition;
 use App\Models\Item;
+use App\Models\Condition;
+use Database\Seeders\CommentSeeder;
+use Database\Seeders\ConditionSeeder;
+use Database\Seeders\ItemSeeder;
+use Database\Seeders\LikeSeeder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ItemTest extends TestCase
@@ -15,8 +18,8 @@ class ItemTest extends TestCase
     /** @test */
     function like_relation() :void
     {
-        $this->seed();
-        $item = Item::withCount('like')->find(1);
+        $this->seed([ItemSeeder::class, LikeSeeder::class]);
+        $item = Item::withCount('like')->first();
         $user_id = [1, 2];
 
         $this->assertInstanceOf(Collection::class, $item->likes);
@@ -28,32 +31,32 @@ class ItemTest extends TestCase
     /** @test */
     function comment_relation() :void
     {
-        $this->seed();
-        $item = Item::withCount('comment')->find(1);
+        $this->seed([ItemSeeder::class, CommentSeeder::class]);
+        $item = Item::withCount('comment')->first();
         $this->assertEquals(3, $item->comment_count);
     }
 
     /** @test */
     function sold_items_relation() :void
     {
-        $this->seed();
-        $item = Item::find(1);
+        $this->seed([ItemSeeder::class]);
+        $item = Item::first();
         $this->assertInstanceOf(Collection::class, $item->sold_items);
     }
 
     /** @test */
     function categories_relation(): void
     {
-        $this->seed();
-        $item = Item::find(1);
+        $this->seed([ItemSeeder::class]);
+        $item = Item::first();
         $this->assertInstanceOf(Collection::class, $item->sold_items);
     }
 
     /** @test */
     function condition_relation(): void
     {
-        $this->seed();
-        $item = Item::find(1);
+        $this->seed([ItemSeeder::class, ConditionSeeder::class]);
+        $item = Item::first();
         $this->assertInstanceOf(Condition::class, $item->condition);
     }
 }
