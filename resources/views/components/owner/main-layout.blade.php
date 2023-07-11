03637b77212db9593e77a19eda9bec04e2833f9a
yu-tooo <teg.moritayuto@gmail.com>
2023-07-11 21:19:52 +0900

    add main-layout

--- resources/views/components/owner/main-layout.blade.php
+++ resources/views/components/owner/main-layout.blade.php
@@ -6,20 +6,45 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>Coach Flma</title>
+  <title>Coach Flma Owners</title>
 
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
+<body x-data="{ open: false }">
   <header class="flex items-center justify-between px-4 h-12 bg-black">
-    <div class="flex items-center">
-      <a href="/">
-        <x-user.application-logo />
+    <div class="flex items-center text-white">
+      <a href="{{ route('owner.home') }}">
+        <x-application-logo />
+      </a>
+      <span class="hidden sm:inline">for</span>
+      <span class="text-xl sm:ml-2">Owner</span>
+    </div>
+    <div class="px-4 space-x-4 hidden sm:block">
+      <a href="{{ route('owner.home') }}'" class="text-white hover:underline">
+        ユーザー
+      </a>
+      <a href="{{ route('owner.home') }}'" class="text-white hover:underline">
+        アイテム
       </a>
     </div>
+    <button @click="open = ! open" class="sm:hidden h-6 w-8 cursor-pointer outline-none">
+      <span class="block w-full h-0.5 bg-white mb-2  duration-500" :class="{'translate-y-2.5': open, 'rotate-45': open}"></span>
+      <span class="block w-full h-0.5 bg-white mb-2  duration-500" :class="{'-translate-x-4': open, 'scale-0': open}"></span>
+      <span class="block w-full h-0.5 bg-white mb-0  duration-500" :class="{'-translate-y-2.5': open, '-rotate-45': open}"></span>
+    </button>
   </header>
-
+  <div :class="{'translate-y-24': open }" class="flex items-center flex-row-reverse absolute px-8 -top-12 bg-black h-12 w-screen duration-500">
+    <a href="{{ route('owner.home') }}" class="text-white mx-4 hover:underline">
+      アイテム
+    </a>
+    <a href="{{ route('owner.home') }}" class="text-white mx-4 hover:underline">
+      ユーザー
+    </a>
+  </div>
+  <div class="mt-8 w-11/12 sm:w-3/4 md:w-1/2 mx-auto">
+    <span class="text-xl font-bold">森田悠斗</span>さん
+  </div>
   <div class="flex flex-col items-center py-8 w-11/12 sm:w-3/4 md:w-1/2 mx-auto">
     {{ $slot }}
   </div>
--- resources/views/owner/index.blade.php
+++ resources/views/owner/index.blade.php
@@ -1,21 +1,6 @@
-<!DOCTYPE html>
-<html lang="">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Owner</title>
-</head>
-<body>
-  <h1>Owner Page</h1>
-  <form action="{{ route('owner.register') }}">
-    <button>登録画面</button>
-  </form>
-  <form action="{{ route('owner.login') }}">
-    <button>ログイン画面</button>
-  </form>
-  <form method="POST" action="{{ route('owner.logout') }}">
-    @csrf
-    <button>ログアウト</button>
-  </form>
-</body>
-</html>
+<x-owner.main-layout>
+  <h1 class="text-lg font-bold">ユーザ一覧</h1>
+  <div>
+    
+  </div>
+</x-owner.main-layout>
