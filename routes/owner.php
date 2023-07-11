6c42ede2469778168212b0f72b0d9c40ea51ba12
yu-tooo <teg.moritayuto@gmail.com>
2023-07-12 01:06:37 +0900

    update owner register test

--- routes/owner.php
+++ routes/owner.php
@@ -7,10 +7,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('guest:owners')->group(function () {
-  Route::get('register', [RegisteredUserController::class, 'create'])
-    ->name('register');
-  Route::post('register', [RegisteredUserController::class, 'store']);
-
   Route::get('login', [AuthenticatedSessionController::class, 'create'])
     ->name('login');
   Route::post('login', [AuthenticatedSessionController::class, 'store']);
@@ -23,4 +19,10 @@ Route::middleware('auth:owners')->group(function() {
     
   Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
     ->name('logout');
+});
+
+Route::middleware('auth:admin')->group(function() {
+  Route::get('register', [RegisteredUserController::class, 'create'])
+    ->name('register');
+  Route::post('register', [RegisteredUserController::class, 'store']);
 });
--- tests/Feature/Http/Controllers/Owner/RegisterdUserControllerTest.php
+++ tests/Feature/Http/Controllers/Owner/RegisterdUserControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers\Owner;
 
+use App\Models\Admin;
 use App\Models\Owner;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -14,32 +15,39 @@ class RegisterdUserControllerTest extends TestCase
     /** @test */
     function owner_registration_view(): void
     {
-        $this->get(route('owner.register'))->assertStatus(200);
+        $this->get(route('owner.register'))->assertRedirect();
+        $admin = Admin::factory()->create();
+        $this->actingAs($admin, 'admin')->get(route('owner.register'))
+        ->assertStatus(200);
     }
 
     /** @test */
     function owner_registration_validate()
     {
         $url = route('owner.register');
+        $admin = Admin::factory()->create();
         Owner::factory()->create(['email' => 'test@example.com']);
 
-        $this->from($url)->post(route('owner.register', []))->assertRedirect($url);
+        $this->actingAs($admin, 'admin')->from($url)->post(route('owner.register', []))->assertRedirect($url);
 
-        $this->post(route('owner.register', ['email' => '']))
+        $this->actingAs($admin, 'admin')->post(route('owner.register', ['email' => '']))
             ->assertInvalid(['email' => 'メールアドレスは必ず入力してください']);
-        $this->post(route('owner.register', ['email' => 'test@example.com']))
+        $this->actingAs($admin, 'admin')->post(route('owner.register', ['email' => 'test@example.com']))
             ->assertInvalid(['email' => 'このメールアドレスは既に存在しています']);
 
-        $this->post(route('owner.register', ['password' => '']))
+        $this->actingAs($admin, 'admin')
+        ->post(route('owner.register', ['password' => '']))
             ->assertInvalid(['password' => 'パスワードは必ず入力してください']);
-        $this->post(route('owner.register', ['password' => 'abc1234']))
+        $this->actingAs($admin, 'admin')
+        ->post(route('owner.register', ['password' => 'abc1234']))
             ->assertInvalid(['password' => 'パスワードは、8文字以上で入力してください']);
     }
 
     /** @test */
     function owner_new_owners_can_register(): void
     {
-        $response = $this->post(route('owner.register'), [
+        $admin = Admin::factory()->create();
+        $response = $this->actingAs($admin, 'admin')->post(route('owner.register'), [
             'name' => 'Test Owner',
             'email' => 'test@example.com',
             'password' => 'password',
