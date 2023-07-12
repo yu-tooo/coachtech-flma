5b22fbce4e9f341d74af02593ec834fa41da3765
yu-tooo <teg.moritayuto@gmail.com>
2023-07-12 14:54:31 +0900

    add home link

--- resources/views/owner/index.blade.php
+++ resources/views/owner/index.blade.php
@@ -2,10 +2,10 @@
   <h1 class="text-lg font-bold mb-8 text-center">ユーザ一覧</h1>
   <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-8 gap-y-8">
     @foreach($users as $user)
-    <div class="flex flex-col items-center">
+    <a href="{{ route('owner.detail', ['user_id' => $user->id]) }}" class="flex flex-col items-center">
       <img src="{{ asset('storage/image/'. $user->profile->getUrl()) }}" class="w-36 h-36 rounded-full object-cover mb-2">
       {{ $user->name }}
-    </div>
+    </a>
     @endforeach
   </div>
 </x-owner.main-layout>
