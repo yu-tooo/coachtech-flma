cb384a2cdd054692647aaf76ef07990d110b7464
yu-tooo <teg.moritayuto@gmail.com>
2023-07-12 20:18:26 +0900

    rename ownercontroller test

---
+++ tests/Feature/Http/Controllers/Owner/UserControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Owner;
+
+use App\Models\Owner;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    function owner_home_view() 
+    {
+        $this->get(route('owner.home'))->assertRedirectToRoute('owner.login');
+
+        $owner = Owner::factory()->create();
+        $users = User::factory(3)->create()->toArray();
+        $this->actingAs($owner, 'owners')->get(route('owner.home'))->assertStatus(200)
+        ->assertSee($owner->name)
+        ->assertSee($users[0]['name'], $users[1]['name'], $users[2]['name']);
+    }
+
+    /** @test */
+    function owner_user_detail() 
+    {
+        $user = User::factory()->create();
+        $this->get(route('owner.user', ['user_id' => $user->id]))
+        ->assertRedirectToRoute('owner.login');
+
+        $this->login('owners');
+        $this->get(route('owner.user', ['user_id' => $user->id]))->assertStatus(200)
+        ->assertSee('プロフィール')
+        ->assertSee($user->name)
+        ->assertDontSee('このユーザを削除する');
+
+        $this->login('admin');
+        $this->get(route('owner.user', ['user_id' => $user->id]))->assertStatus(200)
+        ->assertSee('このユーザを削除する');
+    }
+}
