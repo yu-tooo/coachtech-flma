bc31757519c6532e3507f3c08fc8432e1935d830
yu-tooo <teg.moritayuto@gmail.com>
2023-07-12 11:42:16 +0900

    rename ownercontroller user

--- app/Http/Controllers/Owner/UserController.php
+++ app/Http/Controllers/Owner/UserController.php
@@ -2,17 +2,22 @@
 
 namespace App\Http\Controllers\Owner;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class OwnerController extends Controller
+class UserController extends Controller
 {
     public function index() 
     {
         $ownerName = Auth::guard('owners')->user()->name;
         $users = User::get();
         return view('owner.index', compact('ownerName', 'users'));
+    }
+
+    public function detail()
+    {
+        
     }
 }
