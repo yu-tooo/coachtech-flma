532429b72f3ed0ccb4f818d504dda006a29690c7
yu-tooo <teg.moritayuto@gmail.com>
2023-07-14 15:38:33 +0900

    add email icon

--- resources/views/owner/user.blade.php
+++ resources/views/owner/user.blade.php
@@ -1,7 +1,10 @@
 <x-owner.main-layout :role="$role">
   <div class="mx-auto sm:w-4/5 md:w-3/5 lg:w-2/5 pb-4">
     <h1 class="text-xl font-bold mb-4 text-center">プロフィール</h1>
-    <img src="{{ asset('storage/image/'. $user->profile->getUrl()) }}" class="w-36 h-36 rounded-full object-cover mb-2 mx-auto">
+    <div class="relative w-fit mx-auto mb-2 px-12 ">
+      <x-email-icon class="absolute right-0 bottom-0 stroke-1 cursor-pointer" />
+      <img src="{{ asset('storage/image/'. $user->profile->getUrl()) }}" class="w-36 h-36 rounded-full object-cover">
+    </div>
     <x-owner.readonly labelName='登録番号' :value="$user->id" />
     <x-owner.readonly labelName='名前' :value="$user->name" />
     <x-owner.readonly labelName='郵便番号' :value="$user->profile->getPostCode()" />
